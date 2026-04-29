6f8fff29798c9dd2e89731b622dd0d30057c5522
Pedro Lucas <pl.lucassouza@gmail.com>
2026-04-29 09:30:54 -0300

    Refatorando o código para conexão com o mongodb

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ from core.config import settings
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
+from models.user_model import User
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-  client_db = AsyncIOMotorClient(
-    settings.MONGO_CONNECTION_STRING
-  ).todoapp
+  client = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING)
+  
+  db = client["todoapp"]
   
   await init_beanie(
-    database=client_db,
-    document_models= []
+    database=db,
+    document_models= [
+      User
+    ]
   )
+  
+  print("Conexão com MongoDB (Beanie) inicializada!")
+
+  yield 
+  
+  client.close()
+  print("Conexão com MongoDB encerrada.")
 
 app = FastAPI(
   title=settings.PROJECT_NAME,
-  openapi_url=f"{settings.API_V1_STR}/openapi.json"
+  openapi_url=f"{settings.API_V1_STR}/openapi.json",
+  lifespan=lifespan
 )
 
 
