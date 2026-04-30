2fab10466e074667a1b4d2ed4e920d8e8644b334
Pedro Lucas <pl.lucassouza@gmail.com>
2026-04-30 17:33:58 -0300

    Criando o service

---
+++ services/user_services.py
@@ -0,0 +1,11 @@
+from schemas.user_schema import UserAuth
+from models.user_model import User
+
+class UserService:
+  @staticmethod
+  async def create_user(user: UserAuth):
+    usuario = User(
+      username = user.username,
+      email = user.email,
+      hash_password = user.password
+    )
