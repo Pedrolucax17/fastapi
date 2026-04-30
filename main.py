4a8a7f86506f0548174ed7920d219a36727732b4
Pedro Lucas <pl.lucassouza@gmail.com>
2026-04-30 17:26:30 -0300

    Adicionando Rotas

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
 from models.user_model import User
+from api.api_v1.router import router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -29,6 +30,11 @@ app = FastAPI(
   title=settings.PROJECT_NAME,
   openapi_url=f"{settings.API_V1_STR}/openapi.json",
   lifespan=lifespan
+)
+
+app.include_router(
+  router,
+  prefix=settings.API_V1_STR
 )
 
 
