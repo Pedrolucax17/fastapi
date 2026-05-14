254707dece893b508ce438dbb43de02209351065
Pedro Lucas <pl.lucassouza@gmail.com>
2026-05-14 10:44:51 -0300

    Utilizando o Access Token e Criando Schema Auth

--- api/api_v1/auth/jwt.py
+++ api/api_v1/auth/jwt.py
@@ -3,10 +3,11 @@ from fastapi.security import OAuth2PasswordRequestForm
 from typing import Any
 from services.user_services import UserService
 from core.security import create_acess_token, create_refresh_token
+from schemas.auth_schema import TokenSchema
 
 auth_router = APIRouter()
 
-@auth_router.post("/login")
+@auth_router.post("/login", summary="Cria Acess Token e Refresh Token", response_model=TokenSchema)
 async def login(data: OAuth2PasswordRequestForm = Depends())-> Any:
   usuario = await UserService.authenticate(
     email=data.username,
