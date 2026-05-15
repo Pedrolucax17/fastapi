426103b417014ba88dccc4c076ab8938b29508ce
Pedro Lucas <pl.lucassouza@gmail.com>
2026-05-15 10:41:53 -0300

    Implementando o Refresh Token

--- api/api_v1/auth/jwt.py
+++ api/api_v1/auth/jwt.py
@@ -8,6 +8,11 @@ from schemas.user_schema import UserDetail
 from services.user_services import UserService
 from core.security import create_refresh_token, create_access_token
 from schemas.auth_schema import TokenSchema
+from pydantic import ValidationError
+from core.config import settings
+from schemas.auth_schema import TokenPayload
+from jose import jwt
+from fastapi import Body
 
 auth_router = APIRouter()
 
@@ -31,3 +36,32 @@ async def login(data: OAuth2PasswordRequestForm = Depends())-> Any:
 @auth_router.post('/token-test', summary='Testando o Token', response_model=UserDetail)
 async def test_token(user:User = Depends(get_current_user)):
   return user
+
+@auth_router.post('/refresh', summary='Refresh Token', response_model=TokenSchema)
+async def refresh_token(refresh_token:str = Body(...)):
+  try:
+    payload = jwt.decode(
+      refresh_token,
+      settings.JWT_REFRESH_SECRET_KEY,
+      algorithms=settings.ALGORITHM_SECURITY
+    )
+    token_data = TokenPayload(**payload)
+  except(jwt.JWTError, ValidationError):
+    raise HTTPException(
+      status_code=status.HTTP_403_FORBIDDEN,
+      detail='Token inválido',
+      headers={'WWW-Authenticate': 'Bearer'}
+    )
+  user = await UserService.get_user_by_id(token_data.sub)
+
+  if not user:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Não foi possível encontrar usuário',
+      headers={'WWW-Authenticate': 'Bearer'}
+    )
+
+  return {
+    "access_token": create_access_token(user.user_id),
+    "refresh_token": create_refresh_token(user.user_id)
+  }
