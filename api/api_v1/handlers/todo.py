819bf67de9c5b0a6c84eeaf188ad6d5f613fd6c7
Pedro Lucas <pl.lucassouza@gmail.com>
2026-05-15 15:00:46 -0300

    Criando o schema de todo

--- api/api_v1/handlers/todo.py
+++ api/api_v1/handlers/todo.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, Depends
+from models.user_model import User
+from schemas.todo_schema import TodoDetail
+from api.dependencies.user_deps import get_current_user
 
 todo_router = APIRouter()
 
-@todo_router.get("/test")
-async def test():
-    return 'test'
+@todo_router.get("/", summary="Lista todas as rotas", response_model=List[TodoDetail])
+async def list(current_user:User = Depends(get_current_user)):
+    pass
