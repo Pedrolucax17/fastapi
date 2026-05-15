dfc9fc5092474e3d8d0764f84d3af8f9813f91bb
Pedro Lucas <pl.lucassouza@gmail.com>
2026-05-15 11:03:59 -0300

    Pegando usuario logado

--- api/api_v1/handlers/user.py
+++ api/api_v1/handlers/user.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter, HTTPException, status
+from fastapi.params import Depends
+
+from api.dependencies.user_deps import get_current_user
+from models.user_model import User
 from schemas.user_schema import UserAuth, UserDetail
 from services.user_services import UserService
 import pymongo
@@ -14,3 +18,7 @@ async def adiciona_usuario(data:UserAuth):
       status_code=status.HTTP_400_BAD_REQUEST,
       detail="Username ou Email deste usuario ja existe"
     )
+
+@user_router.get('/me', summary='Detalhes do usuário logado', response_model=UserDetail)
+async def get_me(user: User = Depends(get_current_user)):
+  return user
