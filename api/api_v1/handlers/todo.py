79409277a2a7b3350cefee6384162499d546581c
Pedro Lucas <pl.lucassouza@gmail.com>
2026-05-18 09:23:05 -0300

    Criando o Service

--- api/api_v1/handlers/todo.py
+++ api/api_v1/handlers/todo.py
@@ -1,11 +1,17 @@
 from typing import List
 from fastapi import APIRouter, Depends
 from models.user_model import User
-from schemas.todo_schema import TodoDetail
+from models.todo_model import Todo
+from services.todo_service import TodoService
+from schemas.todo_schema import TodoDetail, TodoCreate
 from api.dependencies.user_deps import get_current_user
 
 todo_router = APIRouter()
 
 @todo_router.get("/", summary="Lista todas as rotas", response_model=List[TodoDetail])
-async def list(current_user:User = Depends(get_current_user)):
-    pass
+async def list_todo(current_user:User = Depends(get_current_user)):
+    return await TodoService.list_todos(current_user)
+
+@todo_router.get("/create", summary="Adicionando Nota",response_model=Todo)
+async def create_todo(data: TodoCreate, current_user:User = Depends(get_current_user)):
+    return await TodoService.create_todo(current_user, data)
---
+++ services/todo_service.py
@@ -0,0 +1,15 @@
+from models.user_model import User
+from models.todo_model import Todo
+from typing import List
+from schemas.todo_schema import TodoCreate
+
+class TodoService:
+    @staticmethod
+    async def list_todos(user: User) -> List[Todo]:
+        todos = await Todo.find(Todo.owner.id == user.id).to_list()
+        return todos
+
+    @staticmethod
+    async def create_todo(user: User, data: TodoCreate) -> Todo:
+        todo = Todo(**data.model_dump(), owner=user)
+        return await todo.insert()
